79d7338475a49fd50a438efda972f4179eac7579
shaarifulz <shaarifulz@gmail.com>
2021-05-01 13:30:27 +0600

    register and show using axios done

--- vuejs/vp1/src/components/HelloWorld.vue
+++ vuejs/vp1/src/components/HelloWorld.vue
@@ -2,12 +2,22 @@
   <div class="hello">
     <h1>App Name: {{ appName }}. Version : {{ version }}</h1>
 
-     <div class="row">
-      <div v-for="(info, index) in infos" :key="index">
-        <h2>{{info.name}}</h2>
-      </div>
-    </div> 
+    <h1>Registration</h1>
 
+    <form>
+      <input type="text" id="name" v-model="userInfo.name" />
+      <input type="number" id="age" v-model="userInfo.age" />
+      <input type="text" id="blood_group" v-model="userInfo.blood_group" />
+      <button @click.prevent="submitData()">Submit</button>
+    </form>
+
+    <button @click="showUserInfos()">Show User</button>
+    <button @click="clearUser()">Clear</button>
+    <div class="row">
+      <div v-for="(info, index) in infos" :key="index">
+        <h5>{{ info.name }}, {{ info.age }}, {{ info.dob }}</h5>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,29 +27,50 @@ export default {
   data() {
     return {
       infos: [],
-      loading: false,
+      userInfo: {
+        name: "",
+        age: 0,
+        blood_group: "",
+      },
     };
   },
-  mounted() {
-    this.getInfo();
-  },
-  methods: {
-    getInfo() {
-      this.loading = false;
 
+  created() {},
+
+  methods: {
+    clearUser() {
+      this.infos = [];
+    },
+    showUserInfos() {
+      (this.infos = []),
+        axios
+          .get("http://localhost:8000/api/info")
+          .then((response) => {
+            this.infos = response.data;
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    },
+    submitData() {
       axios
-        .get("http://localhost:8000/api/info")
+        .post("http://localhost:8000/api/info", {
+          name: this.userInfo.name,
+          age: this.userInfo.age,
+          blood_group: this.userInfo.blood_group,
+          dob: "2021-01-11",
+        })
         .then((response) => {
-          this.infos = response.data;
-          console.log(response.data);
+          console.log(response);
         })
         .catch((error) => {
           console.log(error);
-          this.loading = false;
         });
     },
   },
   name: "HelloWorld",
+  //props used for component: data transfer
   props: {
     appName: String,
     version: String,
@@ -47,5 +78,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
